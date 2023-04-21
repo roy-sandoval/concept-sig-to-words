8e0588f1dbe014e61b8c723287cd1667280220c1
roy-sandoval <roy@roysandoval.com>
2023-04-20 21:49:18 -0700

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,124 +1,158 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const bip39 = require('bip39');
 
 export default function Home() {
+  const sig =
+    '0xb23e7dc2a1b9ee7ca1bbfc62e3d6f04e6e1b956af436c6f90e6e9f0baa7e18a322e1e51758c0e75811d02292e79d47d5462a614e7ca8d6e55088b8781d121080a';
+  const phone = '+523111085816';
+  const [words, setWords] = useState();
+  const [ogWordHash, setOGWordHash] = useState('');
+
+  async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder().encode(message);
+
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // convert bytes to hex string
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('');
+
+    return hashHex;
+  }
+
+  function hex2bin(hex) {
+    hex = hex.replace('0x', '').toLowerCase();
+    let out = '';
+    for (let c of hex) {
+      switch (c) {
+        case '0':
+          out += '0000';
+          break;
+        case '1':
+          out += '0001';
+          break;
+        case '2':
+          out += '0010';
+          break;
+        case '3':
+          out += '0011';
+          break;
+        case '4':
+          out += '0100';
+          break;
+        case '5':
+          out += '0101';
+          break;
+        case '6':
+          out += '0110';
+          break;
+        case '7':
+          out += '0111';
+          break;
+        case '8':
+          out += '1000';
+          break;
+        case '9':
+          out += '1001';
+          break;
+        case 'a':
+          out += '1010';
+          break;
+        case 'b':
+          out += '1011';
+          break;
+        case 'c':
+          out += '1100';
+          break;
+        case 'd':
+          out += '1101';
+          break;
+        case 'e':
+          out += '1110';
+          break;
+        case 'f':
+          out += '1111';
+          break;
+        default:
+          return '';
+      }
+    }
+    return out;
+  }
+
+  function findMnemonic(input) {
+    let words = [];
+
+    const bits_one = input.slice(0, 11);
+    const bits_two = input.slice(253, 264);
+    const bits_three = input.slice(517, 528);
+
+    let bits = [bits_one, bits_two, bits_three];
+
+    for (let co = 0; co < bits.length; co++) {
+      if (co < 3) {
+        let biDec = parseInt(bits[co], 2);
+        words.push(bip39.wordlists.english[biDec]);
+      } else break;
+    }
+    return words;
+  }
+
+  async function createWordHash(words) {
+    const word_1_hash = await sha256(words[0] + phone);
+    const word_2_hash = await sha256(word_1_hash + words[1]);
+    const wordHash = await sha256(word_2_hash + words[2]);
+    return wordHash;
+  }
+
+  async function generateWords() {
+    const shaSig = await sha256(sig);
+    const shaBin = hex2bin(shaSig).substring(0, 12);
+    const binSig = hex2bin(sig);
+    const secret = shaBin + binSig;
+    setWords(findMnemonic(secret));
+    console.log('Generated!');
+  }
+
+  async function checkSecret() {
+    const inputWords = ['bleak', 'course', 'access'];
+    const inputWordHash = await createWordHash(inputWords);
+    if (inputWordHash === ogWordHash) {
+      console.log('TRUE! Sending you your funds!');
+    } else {
+      console.log('Incorrect secret.');
+    }
+  }
+
+  useEffect(() => {
+    if (words) {
+      async function fetchWordHash() {
+        const wordHash = await createWordHash(words);
+        setOGWordHash(wordHash);
+      }
+      fetchWordHash();
+    }
+  }, [words]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div>sig to words</div>
+      <button onClick={generateWords}>Generate secret</button>
+      <button
+        onClick={() => {
+          console.log(words);
+        }}
+      >
+        Log secret
+      </button>
+      <button onClick={checkSecret}>Check secret</button>
     </main>
-  )
+  );
 }
